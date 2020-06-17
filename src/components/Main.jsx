ed7b5c92bb2cf5bfd24c2a1b41302daed9a64565
Chan Myae Maung <maungmaungyoker.mmy@gmail.com>
2020-06-18 02:01:21 +0630

    Redesigned Responsive

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,78 +8,76 @@ import ResultFive from './Results/ResultFive';
 const Main = () => {
 	return (
 		<Fragment>
-			<div className="container">
-				<div className="row">
-					<div className="col-md-10 offset-md-1">
-						<div className="jumbotron text-center">
-							<h1 className="chanCode">အွန်လိုင်းအောင်စားရင်း</h1>
-							<p className="chanCode">
-								ဆယ်တန်းအောင်စားရင်းများကို လွယ်ကူစွာရှာဖွေကြည့်ရှနိုင်ပါသည်။
-							</p>
-						</div>
+			<div className="row">
+				<div className="col-12">
+					<div className="jumbotron text-center ">
+						<h3 className="chanCode">စာမေးပွဲ အောင်စားရင်း</h3>
+						<p className="chanCode">
+							ဆယ်တန်းအောင်စားရင်းများကို လွယ်ကူစွာရှာဖွေကြည့်ရှနိုင်ပါသည်။
+						</p>
 					</div>
 				</div>
+			</div>
 
-				<div className="row">
-					<div className="col-md-10 offset-md-1">
-						<ul className="nav nav-tabs sticky-top bg-light">
-							<li className="nav-item">
-								<a className="nav-link active text-dark" href="#home">
-									၂၀၁၆
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link text-success" href="#menu1">
-									၂၀၁၇
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link text-primary" href="#menu2">
-									၂၀၁၈
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link text-danger" href="#menu4">
-									၂၀၁၉
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link text-info" href="#menu5">
-									၂၀၂၀
-								</a>
-							</li>
-						</ul>
+			<div className="row">
+				<div className="col-12">
+					<ul className="nav nav-tabs sticky-top bg-light mx-auto">
+						<li className="nav-item">
+							<a className="nav-link active text-dark" href="#home">
+								၂၀၁၆
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-success" href="#menu1">
+								၂၀၁၇
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-primary" href="#menu2">
+								၂၀၁၈
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-danger" href="#menu4">
+								၂၀၁၉
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-info" href="#menu5">
+								၂၀၂၀
+							</a>
+						</li>
+					</ul>
 
-						<div className="tab-content">
-							<div id="home" className="container tab-pane active">
-								<ul className="list-group mt-3">
-									<ResultOne />
-								</ul>
-							</div>
-							<div id="menu1" className="container tab-pane fade">
-								<br />
-								<ul className="list-group mt-3" id="year2017">
-									<ResultTwo />
-								</ul>
-							</div>
-							<div id="menu2" className="container tab-pane fade">
-								<br />
-								<ul className="list-group mt-3" id="year2018">
-									<ResultThree />
-								</ul>
-							</div>
-							<div id="menu4" className="container tab-pane fade">
-								<br />
-								<ul className="list-group mt-3" id="year2019">
-									<ResultFour />
-								</ul>
-							</div>
-							<div id="menu5" className="container tab-pane fade">
-								<br />
-								<ul className="list-group mt-3" id="year2020">
-									<ResultFive />
-								</ul>
-							</div>
+					<div className="tab-content">
+						<div id="home" className="container tab-pane active">
+							<ul className="list-group mt-3">
+								<ResultOne />
+							</ul>
+						</div>
+						<div id="menu1" className="container tab-pane fade">
+							<br />
+							<ul className="list-group mt-3" id="year2017">
+								<ResultTwo />
+							</ul>
+						</div>
+						<div id="menu2" className="container tab-pane fade">
+							<br />
+							<ul className="list-group mt-3" id="year2018">
+								<ResultThree />
+							</ul>
+						</div>
+						<div id="menu4" className="container tab-pane fade">
+							<br />
+							<ul className="list-group mt-3" id="year2019">
+								<ResultFour />
+							</ul>
+						</div>
+						<div id="menu5" className="container tab-pane fade">
+							<br />
+							<ul className="list-group mt-3" id="year2020">
+								<ResultFive />
+							</ul>
 						</div>
 					</div>
 				</div>
@@ -87,12 +85,12 @@ const Main = () => {
 
 			<div className="fixed-bottom">
 				<div className="row">
-					<div className="col-md-8 offset-md-2">
+					<div className="col-12">
 						<footer className="bg-light text-center p-3">
-							<p className="lead pt-3">
+							<p className="lead">
 								Developed by : &nbsp;
 								<a
-									className="chan-href text-danger"
+									className="chan-href text-primary"
 									href="fb:/profile/100010530133859"
 									target="_blank"
 									rel="noopener noreferrer"
